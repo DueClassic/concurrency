2e40e7938d196c59856870ac73ee66726c875a72
DueClassic <1219784057@qq.com>
2020-04-30 04:52:24 +0800

    Immutable Example

---
+++ src/main/java/com/dueclassic/concurrency/immutable/ImmutableExample3.java
@@ -0,0 +1,34 @@
+package com.dueclassic.concurrency.immutable;
+
+import com.dueclassic.concurrency.annoations.ThreadSafe;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Slf4j
+@ThreadSafe
+public class ImmutableExample3 {
+
+    private static final ImmutableList<Integer> list=ImmutableList.of(1,2,3);
+
+    private final static ImmutableSet set=ImmutableSet.of(list);
+
+    private final static ImmutableMap<Integer,Integer> map=ImmutableMap.of(1,2,3,4);
+
+    private final static ImmutableMap<Integer,Integer> map1=ImmutableMap.<Integer,Integer>builder()
+            .put(1,2)
+            .put(3,4)
+            .put(5,6)
+            .build();
+
+    public static void main(String[] args) {
+//        set.add(4);
+//        map.put(1,8);
+        System.out.println(map.get(1));
+    }
+}
